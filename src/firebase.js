798d0d5cf98233b2a91a1b94c182e415187aac40
Schpiro <borna.bilandzija@outlook.com>
2026-05-10 18:14:48 +0200

    Add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 // Your Firebase config (from Firebase Console)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCs6twqhkJu7my6zMG67uT88nctsbaRy4A",
+    authDomain: "wedding-form-ba88e.firebaseapp.com",
+    projectId: "wedding-form-ba88e",
+    storageBucket: "wedding-form-ba88e.firebasestorage.app",
+    messagingSenderId: "353674976835",
+    appId: "1:353674976835:web:d3932368a874ff10557ef6"
 };
 
 const app = initializeApp(firebaseConfig);
